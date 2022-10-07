33339dfc13f6156f3ff913b3b84421be97c1e34b
Renan <renan_gonzales@estudante.sc.senai.br>
2022-10-06 21:26:47 -0300

    Inicio

---
+++ RPG/Mago.java
@@ -0,0 +1,21 @@
+public class Mago extends Classe {
+
+    private String classe = "Mago";
+    private int atqBase = 20;
+    private int defBase = 10;
+    private int vida = 200;
+
+    public String getClasse() {
+        return classe;
+    }
+    public int getAtqBase() {
+        return atqBase;
+    }
+    public int getDefBase() {
+        return defBase;
+    }
+    public int getVida() {
+        return vida;
+    }
+    
+}
