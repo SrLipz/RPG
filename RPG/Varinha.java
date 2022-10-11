be7cda6dfd371a3ce6a9ff016d0335740d275d23
Henrique Saturnino <rique_souza2001@hotmail.com>
2022-10-11 17:09:29 -0300

    Mudança na classe de armas de cada personagem

--- RPG/Varinha.java
+++ RPG/Varinha.java
@@ -1,3 +1,33 @@
 public class Varinha extends Mago {
     
+    private String arma = "Varinha";
+    private int atqBaseArma = 16;
+    private int defBaseArma = 9;
+
+    public String getArma() {
+        return arma;
+    }
+    
+    public int getAtqBaseArma() {
+        return atqBaseArma;
+    }
+
+    public int getDefBaseArma() {
+        return defBaseArma;
+    }
+
+    @Override
+    public void atacar () {
+        int atqComArma;
+        atqComArma = getAtqBase() + getAtqBaseArma();
+        System.out.printf("%s usou a arma %s, com uma força total %d.%n", getClasse(), getArma(), atqComArma);
+    }
+    
+    @Override
+    public void defender () {
+        int defComArma;
+        defComArma = getDefBase() + getDefBaseArma();
+        System.out.printf("Guerreiro defendeu com uma força %d.%n", defComArma);
+    }
+
 }
