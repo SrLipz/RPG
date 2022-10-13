7b4f666506519b28cc65c3ada4bb708bc4b3091c
Henrique Saturnino <rique_souza2001@hotmail.com>
2022-10-13 15:48:31 -0300

    alteração classe arqueiro

--- RPG/Arqueiro.java
+++ RPG/Arqueiro.java
@@ -3,14 +3,4 @@ public class Arqueiro extends Personagem {
     public Arqueiro () {
         super("Arqueiro", 160, 20, 30);
     }
-
-    public void arcoLongo () {
-        this.setAtqBase(this.getAtqBase()+12);
-        this.setDefBase(this.getDefBase()+13);
-    }
-
-    public void balestra () {
-        this.setAtqBase(this.getAtqBase()+15);
-        this.setDefBase(this.getDefBase()+10);
-    }
 }
